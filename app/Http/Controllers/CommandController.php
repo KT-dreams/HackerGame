343083a7c68d772b58c4a40c24b84bd4ee69d204
MKathy <kathrine1418@gmail.com>
2020-06-21 16:26:20 +0200

    Command API, Cors added.

---
+++ app/Http/Controllers/CommandController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class CommandController extends Controller
+{
+    public function login()
+    {
+        $data =  request()->json()->all();
+        return 'Hello ' . $data['value'];
+    }
+    
+}
