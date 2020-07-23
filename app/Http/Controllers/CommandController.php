328de776a3f08ef794451454f5238026c666efae
MKathy <kathrine1418@gmail.com>
2020-07-23 13:39:42 +0200

    Login command tests.

--- app/Http/Controllers/CommandController.php
+++ app/Http/Controllers/CommandController.php
@@ -1,12 +1,47 @@
 <?php
-
 namespace App\Http\Controllers;
+
+use Illuminate\Support\Str;
 
 class CommandController extends Controller
 {
+    const MESSAGE_OPTIONS = 'messageOptions';
+
+    public function uuid()
+    {
+        return Str::uuid();
+    }
+
     public function login()
     {
-        $data =  request()->json()->all();
-        return 'Hello ' . $data['value'];
+        $message = request()->json()->all();
+
+        if (array_key_exists(self::MESSAGE_OPTIONS, $message) && array_key_exists('request_uuid', $message[self::MESSAGE_OPTIONS])) {
+            if (array_key_exists('data', $message) && array_key_exists('password', $message['data'])) {
+                $response = [
+                    'data' => 'Hello'
+                ];
+            } else {
+                $response = [
+                    'data' => 'Command requires <password>'
+                ];
+            }
+            return $response;
+        }
+
+        if (array_key_exists('data', $message) && array_key_exists('username', $message['data'])) {
+            return [
+                'data' => '',
+                self::MESSAGE_OPTIONS => [
+                    'request_uuid' => $this->uuid(),
+                    'info' => 'Password for ' . $message['data']['username'] . ':',
+                    'type' => 'password'
+                ]
+            ];
+        }
+
+        return [
+            'data' => 'Login requires <username>'
+        ];
     }
 }
